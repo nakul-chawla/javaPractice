325d620177a9277567d41a4433b75623c3b03fe7
nakul-chawla <500052678@stu.upes.ac.in>
2019-07-06 01:18:11 +0530

    first

---
+++ first.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+class first{
+    static int countSubstring(String a){
+        
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.print(countSubstring(a));
+    }
+}

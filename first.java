95c8506d57213af4d0afc88aa3852970c4c1d91b
nakul-chawla <500052678@stu.upes.ac.in>
2019-07-06 01:22:47 +0530

    correction

--- first.java
+++ first.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class first{
     static int countSubstring(String a){
         int n=a.length();
-        return (n*(n-1))/2;
+        return (n*(n+1))/2;
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);

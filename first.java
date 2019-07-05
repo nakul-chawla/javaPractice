a9a1661c40d83cd6b0f6280ed4f44038f44bd72d
nakul-chawla <500052678@stu.upes.ac.in>
2019-07-06 01:21:32 +0530

    complete

--- first.java
+++ first.java
@@ -1,8 +1,11 @@
+//Find number of substrings of a string
+
 import java.io.*;
 import java.util.Scanner;
 class first{
     static int countSubstring(String a){
-        
+        int n=a.length();
+        return (n*(n-1))/2;
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);

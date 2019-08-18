946c10d5aa061bea6f98cfe1d6c97e40c444ff76
nakul-chawla <500052678@stu.upes.ac.in>
2019-08-19 04:25:03 +0530

    Goldmann

---
+++ func.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class func{
+    public static void  findFactorial(int x){
+        int fact=1;
+        for(int i=2;i<x;i++){
+            fact=(fact*i);
+        }
+        System.out.println(fact);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        findFactorial(x);
+    }
+}
